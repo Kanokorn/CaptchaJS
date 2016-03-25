ae4c04e3ffa6527fe8e9f744c8ae19b2a7a2bb4c
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-25 15:48:30 +0700

    string should be correct pattern

--- src/captcha.js
+++ src/captcha.js
@@ -13,6 +13,30 @@ function Captcha(pattern, leftOperand, operator, rightOperand) {
             return "THREE + 1";
         }
 
+        if (this.leftOperand === 4) {
+            return "FOUR + 1";
+        }
+
+        if (this.leftOperand === 5) {
+            return "FIVE + 1";
+        }
+
+        if (this.leftOperand === 6) {
+            return "SIX + 1";
+        }
+
+        if (this.leftOperand === 7) {
+            return "SEVEN + 1";
+        }
+
+        if (this.leftOperand === 8) {
+            return "EIGHT + 1";
+        }
+
+        if (this.leftOperand === 9) {
+            return "NINE + 1";
+        }
+
         return "ONE + 1";
     }
 }
