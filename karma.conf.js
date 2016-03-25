36036b22550e6f138f205c24ccbd5d1523619a49
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-25 15:26:39 +0700

    use phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/*.js',
-      'src/*.js'
+        'src/*.js',
+        'test/*.js'
     ],
 
 
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
