b4e7c0a91045c41946113b3252dbd3082a3022a1
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-25 15:54:09 +0700

    create integerString

--- src/captcha.js
+++ src/captcha.js
@@ -5,38 +5,50 @@ function Captcha(pattern, leftOperand, operator, rightOperand) {
     this.rightOperand = rightOperand;
 
     this.string = function() {
+        var integerString = {
+            1: "ONE",
+            2: "TWO",
+            3: "THREE",
+            4: "FOUR",
+            5: "FIVE",
+            6: "SIX",
+            7: "SEVEN",
+            8: "EIGHT",
+            9: "NINE"
+        };
+
         if (this.leftOperand === 2) {
-            return "TWO + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 3) {
-            return "THREE + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 4) {
-            return "FOUR + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 5) {
-            return "FIVE + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 6) {
-            return "SIX + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 7) {
-            return "SEVEN + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 8) {
-            return "EIGHT + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
         if (this.leftOperand === 9) {
-            return "NINE + 1";
+            return integerString[this.leftOperand] + " + 1";
         }
 
-        return "ONE + 1";
+        return integerString[this.leftOperand] + " + 1";
     }
 }
